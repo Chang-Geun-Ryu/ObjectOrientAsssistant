73ac5324126ea12dbf4d7e227d790afb84af9026
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-09-13 06:36:01 +0900

    313

---
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Blog.java
@@ -0,0 +1,196 @@
+package academy.pocu.comp2500.assignment1;
+
+import java.util.ArrayList;
+
+public class Blog {
+    private SortingType sortingType = SortingType.ORDER_DATE_DECREASING;
+    private boolean tagFilter = false;
+    private boolean authorFilter = false;
+    private ArrayList<String> tagListToFilter;
+    private ArrayList<String> authorListToFilter;
+    private ArrayList<Post> posts;
+    private ArrayList<Post> filteredPosts;
+
+    public Blog() {
+        posts = new ArrayList<Post>();
+        tagListToFilter = new ArrayList<String>();
+        authorListToFilter = new ArrayList<String>();
+        filteredPosts = new ArrayList<Post>();
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    public void setPostOrder(SortingType sortingType) {
+        this.sortingType = sortingType;
+    }
+
+    public ArrayList<Post> getPostList() {
+        filteredPosts.clear();
+        if (tagFilter && authorFilter) {
+            changePostOrder(this.sortingType);
+            getPostListByAuthorWithTag(authorListToFilter, tagListToFilter);
+        } else if (tagFilter) {
+            changePostOrder(this.sortingType);
+            getPostListByTag(tagListToFilter);
+        } else if (authorFilter) {
+            changePostOrder(this.sortingType);
+            getPostListByAuthor(authorListToFilter);
+        } else {
+            changePostOrder(this.sortingType);
+            for (Post post : posts
+            ) {
+                filteredPosts.add(post);
+            }
+        }
+        return filteredPosts;
+    }
+
+    private void getPostListByTag(ArrayList<String> tags) {
+        for (Post post : posts
+        ) {
+            if (post.matchTag(tags)) {
+                filteredPosts.add(post);
+            }
+        }
+    }
+
+    public void getPostListByAuthor(ArrayList<String> authors) {
+        for (Post post : posts
+        ) {
+            if (authors.contains(post.getAuthor())) {
+                filteredPosts.add(post);
+            }
+        }
+    }
+
+    public void getPostListByAuthorWithTag(ArrayList<String> authors, ArrayList<String> tags) {
+        for (Post post : posts
+        ) {
+            if (authors.contains(post.getAuthor()) && post.matchTag(tags)) {
+                filteredPosts.add(post);
+            }
+        }
+    }
+
+    //이거 밑에 지워야할 수도 있음
+    public void setTagFilter(ArrayList<String> tags) {
+        if (tags == null) {
+            System.out.println("You have to write tag. And I will reset tag filter.");
+            tagFilter = false;
+            tagListToFilter.clear();
+        } else {
+            for (String t : tags
+            ) {
+                tagFilter = true;
+                tagListToFilter.add(t);
+            }
+        }
+    }
+
+    public void setTagFilter(String... tags) {
+        for (String tag : tags
+        ) {
+            if (tag.isBlank()) {
+                System.out.println("You have to write tag. And I will reset tag filter.");
+                tagFilter = false;
+                tagListToFilter.clear();
+            } else {
+                tagFilter = true;
+                tagListToFilter.add(tag);
+            }
+        }
+
+    }
+    /*
+    public void setTagFilter(String tag) {
+        if (tag.isBlank()) {
+            System.out.println("You have to write tag. And I will reset tag filter.");
+            tagFilter = false;
+            tagListToFilter.clear();
+        } else {
+            tagFilter = true;
+            tagListToFilter.add(tag);
+        }
+    }
+    */
+
+    public void clearTagFilter() {
+        tagFilter = false;
+        tagListToFilter.clear();
+    }
+
+    public void setAuthorFilter(User author) {
+        if (author.getNickname().isBlank()) {
+            authorFilter = false;
+            authorListToFilter.clear();
+        } else {
+            authorFilter = true;
+            authorListToFilter.add(author.getNickname());
+        }
+    }
+
+    public void clearAuthorFilter() {
+        authorFilter = false;
+        authorListToFilter.clear();
+    }
+
+    public void changePostOrder(SortingType sortingType) {
+        switch (sortingType) {
+            case ORDER_DATE_INCREASING:
+                posts.sort((post1, post2) -> {
+                    if (post1.getPostTime().compareTo(post2.getPostTime()) < 0) {
+                        return -1;
+                    } else if (post1.getPostTime().compareTo(post2.getPostTime()) > 0) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            case ORDER_DATE_DECREASING:
+                posts.sort((post1, post2) -> {
+                    if (post1.getPostTime().compareTo(post2.getPostTime()) < 0) {
+                        return 1;
+                    } else if (post1.getPostTime().compareTo(post2.getPostTime()) > 0) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                break;
+            case ORDER_REVISION_INCREASING:
+                posts.sort((post1, post2) -> {
+                    if (post1.getModifiedDateTime().compareTo(post2.getModifiedDateTime()) < 0) {
+                        return -1;
+                    } else if (post1.getModifiedDateTime().compareTo(post2.getModifiedDateTime()) > 0) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                break;
+            case ORDER_REVISION_DECREASING:
+                posts.sort((post1, post2) -> {
+                    if (post1.getModifiedDateTime().compareTo(post2.getModifiedDateTime()) < 0) {
+                        return 1;
+                    } else if (post1.getModifiedDateTime().compareTo(post2.getModifiedDateTime()) > 0) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                break;
+            case ORDER_DIC_INCREASING:
+                posts.sort((post1, post2) -> {
+                    if (post1.getTitle().compareTo(post2.getTitle()) > 0) {
+                        return 1;
+                    } else if (post1.getTitle().compareTo(post2.getTitle()) < 0) {
+                        return -1;
+                    }
+                    return 0;
+                });
+                break;
+            default:
+                assert (false) : "Unrecognized sorting type: " + sortingType;
+                break;
+        }
+    }
+}
--- Assignment1/src/academy/pocu/comp2500/assignment1/app/Program.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/app/Program.java
@@ -1,5 +1,6 @@
-package academy.pocu.comp2500.assignment1;
+package academy.pocu.comp2500.assignment1.app;
 
+import academy.pocu.comp2500.assignment1.App;
 import academy.pocu.comp2500.assignment1.registry.Registry;
 
 import java.util.ArrayList;
@@ -8,58 +9,6 @@ public class Program {
 
     public static void main(String[] args) {
 	    // write your code here
-        BlogHost blog = new BlogHost();
-        Content post = new Content("1", "body2", "a1");
-        Content post2 = new Content("2", "body1", "a1");
-        blog.addPost(post);
-        blog.addPost(post2);
-        Content post3 = new Content("3", "body2", "a2");
-        Content post4 = new Content("4", "body1", "a2");
-        blog.addPost(post3);
-        blog.addPost(post4);
-
-        post.addPostTag("t1");
-        post2.addPostTag("t2");
-        post3.addPostTag("t1");
-        post4.addPostTag("t2");
-
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-
-        System.out.println("------");
-        ArrayList<String> tags = new ArrayList<>();
-
-//        tags.add("t1");
-        tags.add("t1");
-//        tags.add("t2");
-        blog.setAuthor("a2");
-        blog.setTags(tags);
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-        System.out.println("------");
-
-        tags.add("t1");
-//        tags.add("t2");
-        blog.setAuthor(null);
-        blog.setTags(null);
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-        System.out.println("------");
-//        tags.add("t1");
-
-        blog.setSortType(BlogHost.SortType.DESCENDINGPOST);
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-        System.out.println("------");
-
-        blog.setSortType(null);
-        blog.setAuthor(null);
-        blog.setTags(null);
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-        System.out.println("------");
-
-        blog.setTitle("a2", "3", "7");
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-        System.out.println("------");
-        blog.setSortType(BlogHost.SortType.ASCENDINGMODIFY);
-        blog.getContents().forEach(e-> System.out.println(e.getTitle()));
-
         Registry registry = new Registry();
         App app = new App(registry);
         registry.validate();
