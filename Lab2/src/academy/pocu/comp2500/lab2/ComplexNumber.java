fa90f89e7a497ec0a5ccc528721496c91e61a162
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-09-20 15:33:28 +0900

    update lab2

---
+++ Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
@@ -0,0 +1,51 @@
+package academy.pocu.comp2500.lab2;
+
+public final class ComplexNumber {
+    public final double real;
+    public final double imaginary;
+    
+    public ComplexNumber() {
+        this.real = 0.0;
+        this.imaginary = 0.0;
+    }
+
+    public ComplexNumber(double real) {
+        this.real = real;
+        this.imaginary = 0.0;
+    }
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public boolean isReal() {
+        return this.imaginary == 0.0;
+    }
+
+    public boolean getImaginary() {
+        return this.real == 0.0;
+    }
+
+    public ComplexNumber getConjugate() {
+        return new ComplexNumber(this.real, -this.imaginary);
+    }
+
+    public ComplexNumber add(ComplexNumber num) {
+        return new ComplexNumber(this.real + num.real, this.imaginary + num.imaginary);
+    }
+
+    public ComplexNumber subtract(ComplexNumber num) {
+        return new ComplexNumber(this.real - num.real, this.imaginary - num.imaginary);
+    }
+
+    public ComplexNumber multiply(ComplexNumber num) {
+        return new ComplexNumber(this.real * num.real - this.imaginary * num.imaginary, this.real * num.imaginary + this.imaginary * num.real);
+    }
+
+    public ComplexNumber divide(ComplexNumber num) {
+        ComplexNumber mul = this.multiply(num.getConjugate());
+        double dTemp = Math.pow(num.real, 2) + Math.pow(num.imaginary, 2);
+        return new ComplexNumber(mul.real / dTemp, mul.imaginary / dTemp);
+    }
+}
