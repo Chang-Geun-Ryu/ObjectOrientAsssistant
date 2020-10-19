90c3ad541b896eacacc51a137e0c6f2268df05d4
Chang-Geun-Ryu <goodrcg@gmail.com>
2020-10-19 20:36:53 +0900

    update

--- Assignment2/src/academy/pocu/comp2500/assignment2/Color.java
+++ Assignment2/src/academy/pocu/comp2500/assignment2/Color.java
@@ -1,25 +1,26 @@
 package academy.pocu.comp2500.assignment2;
 
 public class Color {
-    private int red;
-    private int green;
-    private int blue;
+    private int r;
+    private int g;
+    private int b;
 
     public Color(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
+
+        this.r = Math.min(Math.max(0, r), 0xff);
+        this.g = Math.min(Math.max(0, g), 0xff);
+        this.b = Math.min(Math.max(0, b), 0xff);
     }
 
-    public int getRed() {
-        return red;
+    public int getR() {
+        return r;
     }
 
-    public int getGreen() {
-        return green;
+    public int getG() {
+        return g;
     }
 
-    public int getBlue() {
-        return blue;
+    public int getB() {
+        return b;
     }
 }
